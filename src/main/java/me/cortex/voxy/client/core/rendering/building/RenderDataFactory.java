72aa814b5e91c72b4dfa972b3097508e5c87b066
Haberno <52333828+Haberno@users.noreply.github.com>
2026-03-29 17:51:52 -0400

    fix other issuse and port latest changes

--- src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
@@ -63,8 +63,7 @@ public class RenderDataFactory {
 
     private int quadCount = 0;
 
-    private final OccupancySet occupancy = new OccupancySet();
-
+    private final OccupancySet occupancy;
     //Wont work for double sided quads
     private final class Mesher extends ScanMesher2D {
         public int auxiliaryPosition = 0;
@@ -186,10 +185,17 @@ public class RenderDataFactory {
     private final Mesher seondaryblockMesher = new Mesher();//Used for dual non-opaque geometry
 
     public RenderDataFactory(WorldEngine world, ModelFactory modelManager, boolean emitMeshlets) {
+        this(world, modelManager, emitMeshlets, false);
+    }
+    public RenderDataFactory(WorldEngine world, ModelFactory modelManager, boolean emitMeshlets, boolean generateOccupancy) {
         this.world = world;
         this.modelMan = modelManager;
+        if (generateOccupancy && BUILD_OCCUPANCY_SET) {
+            this.occupancy = new OccupancySet();
+        } else {
+            this.occupancy = null;
+        }
     }
-
     private static long getQuadTyping(long metadata) {//2 bits
         return 0b111L&(0b000_000_010_100L>>(ModelQueries._isTranslucent(metadata)*6+ModelQueries._isDoubleSided(metadata)*3));
     }
@@ -1608,6 +1614,8 @@ public class RenderDataFactory {
         // throwing system works
         this.quadCount = 0;
 
+
+
         {//Reset all the block meshes
             this.blockMesher.reset();
             this.blockMesher.doAuxiliaryFaceOffset = true;
@@ -1625,7 +1633,9 @@ public class RenderDataFactory {
             }
         }
 
-        this.occupancy.reset();
+        if (this.occupancy != null) {
+            this.occupancy.reset();
+        }
 
         this.minX = Integer.MAX_VALUE;
         this.minY = Integer.MAX_VALUE;
@@ -1660,7 +1670,7 @@ public class RenderDataFactory {
         }
 
         //We only care if we have quads
-        if (BUILD_OCCUPANCY_SET && this.quadCount != 0 && (flags&1) != 0) {
+        if (this.occupancy != null && section.lvl == 0 && this.quadCount != 0 && (flags&1) != 0) {
             this.buildOccupancy();
         }
 
@@ -1699,7 +1709,7 @@ public class RenderDataFactory {
         aabb |= (this.maxZ-this.minZ-1)<<25;
 
         MemoryBuffer occupancy = null;
-        if (BUILD_OCCUPANCY_SET && !this.occupancy.isEmpty()) {
+        if (this.occupancy != null && !this.occupancy.isEmpty()) {
             occupancy = new MemoryBuffer(this.occupancy.writeSize());
             this.occupancy.write(occupancy.address, false);
         }
--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRenderer.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRenderer.java
@@ -1,20 +1,12 @@
 package me.cortex.voxy.client.mixin.sodium;
 
+import me.cortex.voxy.commonImpl.VoxyCommon;
 import net.caffeinemc.mods.sodium.client.gl.device.CommandList;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
-import net.caffeinemc.mods.sodium.client.render.chunk.ChunkRenderMatrices;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import me.cortex.voxy.client.core.IGetVoxyRenderSystem;
-import me.cortex.voxy.client.core.rendering.Viewport;
-import me.cortex.voxy.client.core.util.IrisUtil;
-import me.cortex.voxy.commonImpl.VoxyCommon;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderType;
 
 @Mixin(value = SodiumWorldRenderer.class, remap = false)
 public class MixinSodiumWorldRenderer {
@@ -22,34 +14,5 @@ public class MixinSodiumWorldRenderer {
     private void voxy$injectThreadUpdate(CommandList cl, CallbackInfo ci) {
         var vi = VoxyCommon.getInstance();
         if (vi != null) vi.updateDedicatedThreads();
-    }
-
-    @Unique
-    private ChunkRenderMatrices voxy$capturedMatrices;
-
-    @Inject(method = "drawChunkLayer", at = @At("HEAD"))
-    private void voxy$captureMatrices(RenderType renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci) {
-        this.voxy$capturedMatrices = matrices;
-    }
-
-    @Inject(method = "drawChunkLayer", at = @At("TAIL"))
-    private void injectRender(RenderType renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci) {
-        this.doRender(this.voxy$capturedMatrices, renderLayer, x, y, z);
-    }
-    
-    @Unique
-    private void doRender(ChunkRenderMatrices matrices, RenderType renderLayer, double x, double y, double z) {
-        if (renderLayer == RenderType.solid()) {
-            var renderer = ((IGetVoxyRenderSystem) Minecraft.getInstance().levelRenderer).getVoxyRenderSystem();
-            if (renderer != null) {
-                Viewport<?> viewport = null;
-                if (IrisUtil.irisShaderPackEnabled()) {
-                    viewport = renderer.getViewport();
-                } else {
-                    viewport = renderer.setupViewport(matrices, x, y, z);
-                }
-                renderer.renderOpaque(viewport);
-            }
-        }
     }
 }
